55c2de46030aa3dab9cf8d70de06fdc6992f239f
HanNguyen0910 <anguyenlebaohan@gmail.com>
2024-09-04 09:19:52 +0700

    frontend call api

--- src/components/GridDisplay.js
+++ src/components/GridDisplay.js
@@ -1,65 +1,39 @@
-import React from "react";
-import { Box, SimpleGrid, Image } from "@chakra-ui/react";
+import React from 'react';
+import { Box, SimpleGrid, Image, Text } from '@chakra-ui/react';
 
-function GridDisplay () {
-
-    return(
-        <Box mt={8} width="100%">
-          <SimpleGrid columns={5} spacing={2} width="100%" py='6'>
-            {/* Sample Keyframe 1 */}
-            <Box width="100%" height="150px" borderWidth="1px" borderRadius="md" overflow="hidden">
+function GridDisplay({ searchResults }) {
+  return (
+    <Box mt={8} width="100%">
+      {/* SimpleGrid to display thumbnails in a grid format */}
+      <SimpleGrid columns={[1, 2, 3, 4, 5]} spacing={4} width="100%">
+        {searchResults.length > 0 ? (
+          searchResults.map((result, index) => (
+            <Box
+              key={index}
+              borderWidth="1px"
+              borderRadius="md"
+              overflow="hidden"
+              display="flex"
+              alignItems="center"
+              justifyContent="center"
+              bg="gray.100"
+              height="150px"
+            >
               <Image
-                src="https://images.unsplash.com/photo-1534330980656-d201223895ee?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                alt="Keyframe 1"
+                src={result.thumbnail} // Thumbnail URL from search results
+                alt={`Thumbnail for video ${result.video_id}`}
+                objectFit="cover"
                 width="100%"
                 height="100%"
-                objectFit="cover"
               />
             </Box>
-
-            {/* Sample Keyframe 2 */}
-            <Box width="100%" height="150px" borderWidth="1px" borderRadius="md" overflow="hidden">
-              <Image
-                src="https://cdn.pixabay.com/photo/2024/02/28/07/42/european-shorthair-8601492_640.jpg"
-                alt="Keyframe 1"
-                width="100%"
-                height="100%"
-                objectFit="cover"
-              />
-            </Box>
-
-            <Box width="100%" height="150px" borderWidth="1px" borderRadius="md" overflow="hidden">
-              <Image
-                src="https://images.unsplash.com/photo-1534330980656-d201223895ee?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                alt="Keyframe 1"
-                width="100%"
-                height="100%"
-                objectFit="cover"
-              />
-            </Box>
-
-            <Box width="100%" height="150px" borderWidth="1px" borderRadius="md" overflow="hidden">
-              <Image
-                src="https://images.unsplash.com/photo-1534330980656-d201223895ee?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                alt="Keyframe 1"
-                width="100%"
-                height="100%"
-                objectFit="cover"
-              />
-            </Box>
-
-            <Box width="100%" height="150px" borderWidth="1px" borderRadius="md" overflow="hidden">
-              <Image
-                src="https://images.unsplash.com/photo-1534330980656-d201223895ee?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                alt="Keyframe 1"
-                width="100%"
-                height="100%"
-                objectFit="cover"
-              />
-            </Box>
-          </SimpleGrid>
-        </Box>
-    )
+          ))
+        ) : (
+          <Text>No results found.</Text>
+        )}
+      </SimpleGrid>
+    </Box>
+  );
 }
 
 export default GridDisplay;
