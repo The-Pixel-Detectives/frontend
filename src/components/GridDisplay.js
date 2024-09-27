b6e9bc809e2955a3594986193125bab751844333
Minh Anh <minhanhng.sofl@gmail.com>
2024-09-27 14:13:13 +0700

    Update metadata

--- src/components/GridDisplay.js
+++ src/components/GridDisplay.js
@@ -6,14 +6,15 @@ import FrameResult from "./FrameResult";
 
 function GridDisplay({ searchResults }) {
 
-  const handleVideoClick = async (videoID) => {
-    console.log("Click!")
-    try {
-      await openVideo(videoID, '200');
-    } catch (error) {
-      console.error('Failed to download file', error);
-    }
-  };
+	const secondsToHms = (d) => {
+    d = Number(d);
+    var m = Math.floor(d % 3600 / 60);
+    var s = Math.floor(d % 3600 % 60);
+
+		var mDisplay = m > 0 ? m + "m" : "";
+    var sDisplay = s > 0 ? s + "s" : "";
+    return mDisplay + sDisplay;
+	}
 
   return (
     <Box mt={8} width="100%">
@@ -26,12 +27,15 @@ function GridDisplay({ searchResults }) {
             overflow="hidden"
             bg="gray.100"
           >
-            <Text>Video ID: {result.video_id}</Text>
-            <Text>Frame Indexes: {result.frame_indices.join(', ')}</Text>
-            <Text>Timestamps: {result.timestamps.join(', ')}</Text>
-            {/* <Text>Key Frame: {result.keyframes.join(', ')}</Text> */}
-            <Text>Score: {result.score}</Text>
-            <Text>Local file path: {result.local_file_path}</Text>
+						<Box padding={2}>
+              <Text>#{index} Video ID: <strong>{result.video_id}</strong></Text>
+							<Text>Timestamps: {result.timestamps.map(item => secondsToHms(item)).join(', ')}</Text>
+							<Text>Frame Indexes: {result.frame_indices.join(', ')}</Text>
+              <Text>FPS: {result.fps}</Text>
+							{/* <Text>Key Frame: {result.keyframes.join(', ')}</Text> */}
+							{/* <Text>Score: {result.score}</Text> */}
+							{/* <Text>Local file path: {result.local_file_path}</Text> */}
+						</Box>
 						<Stack direction="row" gap={0} wrap="wrap" width="100%">
 							{result.frame_indices.map((value, index) => <FrameResult
 							group_id={result.group_id}
